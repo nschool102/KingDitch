ceae63e2da6d2c389eea0516aeb3bf527dccd56e
nschool102 <34006773+nschool102@users.noreply.github.com>
2026-06-09 16:00:45 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = "app-cache-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./build.txt", 
+  "./image/icon.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+      )
+    )
+
+  );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, clone));
+
+        return response;
+
+      })
+
+      .catch(() => caches.match(event.request))
+
+  );
+
+});
